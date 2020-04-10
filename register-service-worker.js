be36e2136392098c596e9650f4f031c10a690d7e
Amol Bhave <amol@amolbhave.com>
2020-04-10 18:37:45 -0400

    Updates

---
+++ register-service-worker.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('/all-aboard-client/expo-service-worker.js', { scope: '/all-aboard-client/' })
+      .then(function(info) {
+        // console.info('Registered service-worker', info);
+      })
+      .catch(function(error) {
+        console.info('Failed to register service-worker', error);
+      });
+  });
+}
